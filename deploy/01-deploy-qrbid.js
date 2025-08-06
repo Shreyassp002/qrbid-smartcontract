f31542b7dc1bd69cad605f5c73942b6fa0125bbe
Shreyas Patil <shreyassp002@gmail.com>
2025-08-06 11:48:18 +0530

    Implemented deploy script for QRBid

---
+++ deploy/01-deploy-qrbid.js
@@ -0,0 +1,40 @@
+const { network, ethers } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    log("----------------------------------------------------")
+    log("Deploying QRBid contract...")
+    log("Network:", network.name)
+    log("Chain ID:", chainId)
+    log("Deployer:", deployer)
+
+    // Platform wallet - can be deployer or different address
+    const platformWallet = deployer // Change this if needed
+    
+    const args = [platformWallet]
+
+    const qrBid = await deploy("QRBid", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+
+    log("QRBid deployed to:", qrBid.address)
+    log("Platform wallet:", platformWallet)
+
+    // Verify contract on live networks
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying contract...")
+        await verify(qrBid.address, args)
+    }
+    
+    log("----------------------------------------------------")
+}
+
+module.exports.tags = ["all", "qrbid"]
